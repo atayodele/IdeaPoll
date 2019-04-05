ccbb3517c841a1f2263425e0b2ea6d80700018ff
atayodele <atayodele555@gmail.com>
2019-04-05 10:55:23 +0100

    add migration

---
+++ IdeaSolution.Data/Migrations/20190405093527_UpdateUserModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdeaSolution.Data.Migrations
+{
+    public partial class UpdateUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Firstname",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Firstname",
+                table: "AspNetUsers");
+        }
+    }
+}
