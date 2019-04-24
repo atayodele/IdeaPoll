74b3947766427dec5ad218a9101962f018f5126d
atayodele <atayodele555@gmail.com>
2019-04-24 12:48:28 +0100

    Update doestnotcontainpassword

--- IdeaSolution.Data/IGeneric/Auth/DoesNotContainPasswordValidator.cs
+++ IdeaSolution.Data/IGeneric/Auth/DoesNotContainPasswordValidator.cs
@@ -11,10 +11,10 @@ namespace IdeaSolution.Data.IGeneric.Auth
         public async Task<IdentityResult> ValidateAsync(UserManager<TUser> manager, TUser user, string password)
         {
             //var username = await manager.GetUserNameAsync(user);
-            //var email = await manager.GetEmailAsync(user);
+            var email = await manager.GetEmailAsync(user);
 
-            //if (email == password)
-            //    return IdentityResult.Failed(new IdentityError { Description = "Password cannot contain username" });
+            if (email == password)
+                return IdentityResult.Failed(new IdentityError { Description = "Password cannot contain username" });
             if (password.Contains("password"))
                 return IdentityResult.Failed(new IdentityError { Description = "Password cannot conain password" });
 
